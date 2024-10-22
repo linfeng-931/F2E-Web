4e89590e899546546e3de98f5b44821966178c6c
linfeng-931 <s111219030@stu.ntue.edu.tw>
2024-10-22 10:48:10 +0800

    細節修正

---
+++ 分頁/script.js
@@ -0,0 +1,37 @@
+const main = document.querySelector(".slider-container");
+const slideWrapper = document.querySelector(".slide-wrapper");
+const slides = document.querySelector(".slider");
+const btns = document.querySelector(".btn");
+
+let currentIndex = 0;
+
+function initializeSlide(){
+    slides[0].style.opacity = "1";
+    slides[0].classList.add("active");
+}
+function updateSlide(){
+    const mainWidth = main.offsetWidth;
+    const translateValue = currentIndex * -mainWidth;
+    slideWrapper.style.transform = `translateX(${translateValue}px)`
+
+    btns.forEach((btn, index) => {
+        btn.classList.toggle("active", index === currentIndex);
+      });
+
+    slides.forEach((slide, index) => {
+        slide.style.opacity = index === currentIndex ? "1" : "0";
+        slide.classList.toggle("active", index === currentIndex);
+    });
+}
+
+btns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      currentIndex = index;
+      updateSlide();
+    });
+  });
+
+window.addEventListener("resize", () => {
+    updateSlide();
+});
+
