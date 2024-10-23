065802eab7f7fdc5fe94f462a6d5b3718f8d5a28
jen067 <dudu671194@gmail.com>
2024-10-24 01:22:46 +0800

    新增塑膠介紹的gsap效果

---
+++ 主頁/script.js
@@ -0,0 +1,21 @@
+gsap.set(".photo img", { opacity: 0, scale: 0.5 });
+gsap.set(".photo:first-child img", { opacity: 1, scale: 1 }); // 第一張圖預設顯示
+
+const animation = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".gallery",
+    start: "1rem top",
+    end: "bottom bottom",
+    pin: ".gallery-right", // 固定右側容器
+    scrub: true,
+    markers: true,
+  },
+});
+
+// 每一張圖片逐次淡入淡出，上一張圖片會逐漸淡出
+animation
+  .to(".photo:nth-child(1) img", { opacity: 0, scale: 0.5, duration: 1 })
+  .to(".photo:nth-child(2) img", { opacity: 1, scale: 1, duration: 1 }) // "-=1" 確保前後動畫同步進行
+  .to(".photo:nth-child(2) img", { opacity: 0, scale: 0.5, duration: 1 })
+  .to(".photo:nth-child(3) img", { opacity: 1, scale: 1, duration: 1 });
+//   .to(".photo:nth-child(3) img", { opacity: 0, scale: 0.5, duration: 1 });
