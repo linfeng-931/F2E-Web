0394667fb3ab28c312f431d439065b3b08a4f361
jen067 <dudu671194@gmail.com>
2024-10-24 07:39:30 +0800

    新增humanity區域內容

--- 主頁/script.js
+++ 主頁/script.js
@@ -4,18 +4,17 @@ gsap.set(".photo:first-child img", { opacity: 1, scale: 1 }); // 第一張圖預
 const animation = gsap.timeline({
   scrollTrigger: {
     trigger: ".gallery",
-    start: "1rem top",
+    start: "top -1.5rem",
     end: "bottom bottom",
     pin: ".gallery-right", // 固定右側容器
     scrub: true,
-    markers: true,
+    markers: false,
   },
 });
 
 // 每一張圖片逐次淡入淡出，上一張圖片會逐漸淡出
 animation
   .to(".photo:nth-child(1) img", { opacity: 0, scale: 0.5, duration: 1 })
-  .to(".photo:nth-child(2) img", { opacity: 1, scale: 1, duration: 1 }) // "-=1" 確保前後動畫同步進行
+  .to(".photo:nth-child(2) img", { opacity: 1, scale: 1, duration: 1 })
   .to(".photo:nth-child(2) img", { opacity: 0, scale: 0.5, duration: 1 })
   .to(".photo:nth-child(3) img", { opacity: 1, scale: 1, duration: 1 });
-//   .to(".photo:nth-child(3) img", { opacity: 0, scale: 0.5, duration: 1 });
