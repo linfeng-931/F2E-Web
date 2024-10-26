976d4065b33afbf60fa034ce8f7de3e001caab30
jen067 <dudu671194@gmail.com>
2024-10-26 20:30:01 +0800

    Perpetrators 樣式設定

--- 主頁/script.js
+++ 主頁/script.js
@@ -1,20 +1,18 @@
-gsap.set(".photo img", { opacity: 0, scale: 0.5 });
-gsap.set(".photo:first-child img", { opacity: 1, scale: 1 }); // 第一張圖預設顯示
+gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0 });
 
-const animation = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".gallery",
-    start: "top -1.5rem",
-    end: "bottom bottom",
-    pin: ".gallery-right", // 固定右側容器
-    scrub: true,
-    markers: false,
-  },
+const animation = gsap.to(".photo:not(:first-child)", {
+  opacity: 1,
+  scale: 1,
+  duration: 1,
+  stagger: 1,
 });
 
-// 每一張圖片逐次淡入淡出，上一張圖片會逐漸淡出
-animation
-  .to(".photo:nth-child(1) img", { opacity: 0, scale: 0.5, duration: 1 })
-  .to(".photo:nth-child(2) img", { opacity: 1, scale: 1, duration: 1 })
-  .to(".photo:nth-child(2) img", { opacity: 0, scale: 0.5, duration: 1 })
-  .to(".photo:nth-child(3) img", { opacity: 1, scale: 1, duration: 1 });
+ScrollTrigger.create({
+  trigger: ".gallery",
+  start: "top -160rem",
+  end: "bottom bottom",
+  pin: ".gallery-right",
+  animation: animation,
+  scrub: true,
+  markers: true,
+});
