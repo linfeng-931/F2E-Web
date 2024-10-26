3a3689fef2914fc2053e0b6388189765b4d833c9
jen067 <dudu671194@gmail.com>
2024-10-27 02:39:34 +0800

    改動perpetration排版

--- 主頁/script.js
+++ 主頁/script.js
@@ -1,18 +1,46 @@
-gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0 });
+const container = document.querySelector(".container");
+const sections = gsap.utils.toArray(".container section");
+const texts = gsap.utils.toArray(".anim");
+const mask = document.querySelector(".mask");
 
-const animation = gsap.to(".photo:not(:first-child)", {
-  opacity: 1,
-  scale: 1,
-  duration: 1,
-  stagger: 1,
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: "+=3200",
+    //snap: 1 / (sections.length - 1),
+    markers: true,
+  },
 });
 
-ScrollTrigger.create({
-  trigger: ".gallery",
-  start: "top -150rem",
-  end: "bottom bottom",
-  pin: ".gallery-right",
-  animation: animation,
-  scrub: true,
-  markers: true,
+gsap.to(mask, {
+  width: "100%",
+  scrollTrigger: {
+    trigger: ".wrapper",
+    start: "top left",
+    scrub: 1,
+  },
+});
+
+sections.forEach((section) => {
+  let text = section.querySelectorAll(".anim");
+
+  if (text.length === 0) return;
+
+  gsap.from(text, {
+    // y: 130,
+    opacity: 0,
+    duration: 2,
+    ease: "elastic",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      start: "left center",
+      markers: true,
+    },
+  });
 });
