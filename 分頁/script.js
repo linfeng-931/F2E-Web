3071caca4aa7545129ac2a59ed0f13cda0374879
linfeng-931 <s111219030@stu.ntue.edu.tw>
2024-10-26 16:24:19 +0800

    新增頁內索引標籤

--- 分頁/script.js
+++ 分頁/script.js
@@ -3,6 +3,7 @@ const slide = document.querySelectorAll(".slide");
 const slideRow = document.getElementById("slide-wrapper");
 const main = document.querySelector(".slider-container");
 const imgBtn = document.querySelectorAll(".img-wrapper a");
+const tocBtn = document.querySelectorAll("table-of-contents a");
 
 let currentIndex = 0;
 
@@ -57,3 +58,39 @@ imgBtn.forEach((e) => {
 window.addEventListener("resize", () => {
   updateSlide();
 });
+
+// 頁面目錄的點擊事件
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll(".table-of-contents .toc"); // 獲取所有鏈接
+
+  links.forEach(link => { // 使用 forEach 遍歷所有鏈接
+    link.addEventListener("click", (event) => {
+      event.preventDefault(); // 防止默認跳轉行為
+
+      const id = link.getAttribute("data-id"); // 使用 link 來獲取 data-id
+      const targetElement = document.getElementById("area" + id);
+      const targetTop = targetElement.getBoundingClientRect().top + window.scrollY; // 取得目標區塊的 y 座標
+
+      if (window.innerWidth < 1920) { // 1440
+        if (targetElement) { // 檢查 targetElement 是否存在
+          window.scrollTo({ // 平滑滾動到目標位置
+            top: (targetTop-150),
+            behavior: "smooth"
+          });
+        }
+      } 
+      else if (window.innerWidth < 2000) { // 1990
+        if (targetElement) { // 檢查 targetElement 是否存在
+          window.scrollTo({ // 平滑滾動到目標位置
+            top: (targetTop-270),
+            behavior: "smooth"
+          });
+      }
+
+      
+      };
+    });
+  });
+});
+
+
