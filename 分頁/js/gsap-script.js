f96b9b2464e86b220176e866566a5856c2e1573a
linfeng-931 <s111219030@stu.ntue.edu.tw>
2024-12-10 22:28:15 +0800

    plastic-bags分頁動畫與RWD、donate清空輸入框功能修復

--- 分頁/js/gsap-script.js
+++ 分頁/js/gsap-script.js
@@ -157,7 +157,19 @@ gsap.from(".animal-intro-section", {
       markers: false,
     },
   });
-  
+  gsap.from("#fishingnet-link", {
+    y: 100,
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+      trigger: "#fishingnet-link",
+      start: "top 60%",
+      end: "top 30%",
+      scrub: 1,
+      markers: false,
+    },
+  });
+
   //data
   const dataColor = document.querySelector('.data-color-1');
   const observer = new IntersectionObserver((entries, observer) => {
@@ -182,12 +194,39 @@ gsap.from(".animal-intro-section", {
             duration: 4, 
           }
         );
+        gsap.fromTo("#page-2 .data-color-2", 
+          { 
+            width: 0,  
+          },
+          { 
+            width: "80%", 
+            duration: 4, 
+          }
+        );
         gsap.fromTo(".data-color-3", 
           { 
             width: 0,  
           },
           { 
             width: "60%", 
+            duration: 4, 
+          }
+        );
+        gsap.fromTo("#page-2 .data-color-3", 
+          { 
+            width: 0,  
+          },
+          { 
+            width: "40%", 
+            duration: 4, 
+          }
+        );
+        gsap.fromTo("#page-2 .data-color-4", 
+          { 
+            width: 0,  
+          },
+          { 
+            width: "35%", 
             duration: 4, 
           }
         );
